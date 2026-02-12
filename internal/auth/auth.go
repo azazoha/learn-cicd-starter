4b85b61339c9fde7dd669ea8a3f399135c23e398
Zagir <azazoha@gmail.com>
2026-02-12 14:49:56 +0400

    adding an error to check for failure

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,7 +12,7 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "", ErrNoAuthHeaderIncluded
+		##return "", ErrNoAuthHeaderIncluded
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
@@ -21,3 +21,5 @@ func GetAPIKey(headers http.Header) (string, error) {
 
 	return splitAuth[1], nil
 }
+
+
